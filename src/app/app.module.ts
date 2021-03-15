0e030bf77b34ca518bf55b493788c12830ea1180
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-03-15 09:55:06 -0600

    finished unit 5, added departments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { DepartmentsComponent } from './components/departments/departments.component';
 import { MaterialModule } from './modules/material.module';
 
 @NgModule({
@@ -16,7 +17,8 @@ import { MaterialModule } from './modules/material.module';
     TimesheetComponent,
     AnalyticsComponent,
     TopNavbarComponent,
-    AnalyticsTableComponent
+    AnalyticsTableComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
