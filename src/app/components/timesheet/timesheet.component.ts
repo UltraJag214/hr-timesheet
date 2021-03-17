ed604d3145fdcf653db9a35cc34b0df3ef3c57fe
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-03-17 10:01:52 -0600

    added template driven forms

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -16,6 +16,9 @@ export class TimesheetComponent implements OnInit {
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
+  weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+
 
   constructor(
     private route: ActivatedRoute,
@@ -36,11 +39,19 @@ export class TimesheetComponent implements OnInit {
         departmentId: this.department.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0
       });
 
       this.employeeNameFC.setValue('');
     }
   }
+
 
   nameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
@@ -54,6 +65,15 @@ export class TimesheetComponent implements OnInit {
       }
       return error;
     };
+  }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+      + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
   }
 
 }
