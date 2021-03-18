cf4304ad07589f23cd6d66dbfe4e41b4519b0065
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-03-18 08:49:23 -0600

    added async pipe refactor

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from 'src/app/interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[];
+  $departments: Observable<Department[]>;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,9 +19,11 @@ export class DepartmentsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.departmentsService.getDeparments().subscribe(departments => {
-      this.departments = departments;
-    });
+    // this.departmentsService.getDeparments().subscribe(departments => {
+    //   this.departments = departments;
+    // });
+
+    this.$departments = this.departmentsService.getDeparments();
   }
 
   goToDepartment(departmentId: string): void {
